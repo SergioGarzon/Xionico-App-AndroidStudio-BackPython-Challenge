2c22bb8693c0db88d7f8a1b31457ffbb15058873
Sergio Garzon <sggarzonutn@gmail.com>
2025-11-01 11:11:32 -0300

    fix: Se eliminan carpetas del proyecto

--- Xionico-app/app/src/main/java/com/example/xionico_app/data/models/HouseAPICallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.xionico_app.data.models;
-
-import java.util.List;
-
-public interface HouseAPICallback {
-    void onSuccess(List<House> houseList);
-    void onError(String errorMessage);
-}
